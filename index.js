7d8e5f1be57d5d9d7566bf1d854d51918781c29c
Ashlee Scott Gregory <ashlee.scott@gmail.com>
2022-12-16 20:29:38 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Oscar) {
+    cats.push(Oscar)
+}
+
+function destructivelyPrependCat(Homer) {
+    cats.unshift(Homer)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+   
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(Joseph) {
+    return [...cats, Joseph]
+}
+
+function prependCat(Miles) {
+    return [Miles,...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1)
+}
+
+function removeFirstCat () {
+    return cats.slice(1)
+}
